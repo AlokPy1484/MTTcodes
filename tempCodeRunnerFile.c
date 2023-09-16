1999a5b6aa0e31841b0f0bc4882722be3f8439ac
AlokPy1484 <alokpandey1483@gmail.com>
2023-09-16 14:19:25 +0530

    fresh

--- tempCodeRunnerFile.c
+++
@@ -1,4 +0,0 @@
-    // for (int i = 0; i < 8; i++) {
-    //     printf("Enter value #%d: ", i + 1);
-    //     scanf("%d", &array[i]); // Read an integer from the user and store it in the array
-    // }
